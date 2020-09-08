10b9648e344241770e8d3e4a6f4cccde5c4db061
gunnmgirl <48892214+gunnmgirl@users.noreply.github.com>
2020-09-08 12:57:53 +0200

    Adds user model and signup controller function

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+
+import authRoutes from "./routes/authRoutes";
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.use("/auth", authRoutes);
+
+app.use((error, req, res, next) => {
+  res.status(error.statusCode).send(error.message);
+});
+
+mongoose
+  .connect(process.env.CONNECTION_STRING, { useUnifiedTopology: true })
+  .then((result) => {
+    console.log("Connected to a server!");
+    app.listen(4000);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
---
+++ src/routes/authRoutes.js
@@ -0,0 +1,30 @@
+import express from "express";
+import { body } from "express-validator/check";
+
+import authController from "../controllers/authController";
+import User from "../models/userModel";
+
+const router = express.Router();
+
+router.put(
+  "/signup",
+  [
+    body("email")
+      .isEmail()
+      .withMessage("Please enter a valid email")
+      .custom((value, { req }) => {
+        return User.findOne({ email: value }).then((user) => {
+          if (user) {
+            return Promise.reject("Email already exists!");
+          }
+        });
+      }),
+    body("password")
+      .trim()
+      .isLength({ min: 5 })
+      .withMessage("Must be at least 5 characters long"),
+  ],
+  authController.signup
+);
+
+export default router;
